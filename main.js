6a74aad61b9b7f406322f3fbedced80e9691f8f3
KatariiDev <giaphu.khh@gmail.com>
2024-04-24 12:02:01 +0700

    First

---
+++ main.js
@@ -0,0 +1,45 @@
+// Nội dung thiệp
+var letterContent = 
+"Đây là nội dung của bức thư mà người ấy sẽ đọc được. Hãy viết gì đó cho người đó thôi nào. Cảm ơn bạn đã xem"
+
+// Typing Speed (Khuyến khích value:75) - (value càng GIẢM speed càng CAO)
+durationWrite = 40 
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
